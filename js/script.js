4e4dd00308b6548621da705bc951b0050edea600
Rosamund <rosamund.pearce@carbonbrief.org>
2018-05-08 15:56:23 +0100

    Change basemap to dark matter style

---
+++ js/script.js
@@ -0,0 +1,11 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXhhbXBsZXMiLCJhIjoiY2lqbmpqazdlMDBsdnRva284cWd3bm11byJ9.V6Hg2oYJwMAxeoR9GEzkAA';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'https://openmaptiles.github.io/dark-matter-gl-style/style-cdn.json',
+    center: [-7, 54],
+    zoom: 5
+});
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
