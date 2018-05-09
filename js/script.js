c95d6f032138db2cafedb39e23c24a8d8d19708d
Rosamund Pearce <rosamundpearce@live.com>
2018-05-09 11:46:26 +0100

    Make scale linear

--- js/script.js
+++ js/script.js
@@ -9,3 +9,63 @@ var map = new mapboxgl.Map({
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
+
+// Store an array of quantiles
+var max = 6720;
+var fifth = 6720 / 5;
+var quantiles = [];
+for (i = 0; i < 5; i++) {
+  var quantile = (fifth + i) * fifth;
+  quantiles.push(quantile);
+}
+
+map.on('load', function() {
+
+    map.addLayer({
+        id: 'powerplants',
+        type: 'circle',
+        source: {
+          type: 'geojson',
+          data: './data/plants.geojson'
+        },
+        paint: {
+            'circle-radius': {
+                property: 'capacity',
+                type: 'exponential', // exponential scales in mapbox default to 1 which is a linear scale
+                stops: [
+                  [60, 4],
+                  [6720, 30]
+                ]
+              },
+            // 'circle-radius': [
+            //     'interpolate',
+            //     ['linear'],
+            //     ['number', ['get', 'capacity']],
+            //     // first number is the capacity and second is the size
+            //     0, 1,
+            //     10, 3,
+            //     100, 6,
+            //     1000, 9,
+            //     10000, 12
+            //     ],
+          'circle-color': [
+            'match',
+            ['get', 'type'],
+            "Coal", "#ced1cc",
+            "Gas", "#4e80e5",
+            "Solar", "#ffc83e",
+            "Nuclear", "#dd54b6",
+            "Oil", "#a45edb",
+            "Hydro", "#43cfef",
+            "Wind", "#00a98e",
+            "Biomass", "#A7B734",
+            "Waste", "#ea545c",
+            "Other", "#cc9b7a",
+            /* other */ '#ffc83e'
+          ],
+          'circle-opacity': 0.45
+        }
+        //'filter': ['all', filterStartYear, filterEndYear, filterType]
+      });
+
+});
