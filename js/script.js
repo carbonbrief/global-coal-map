6ea048bdd4064eadd7c8a2a8e1c2a93da8349698
Rosamund Pearce <rosamundpearce@live.com>
2018-05-11 11:50:05 +0100

    add mouseevents to all layers

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ map.on('load', function() {
 
     var year = 2017;
 
-    // set up three different filters for the three different colours
+    // set different filters for different colours
     // plants about to close
     var filterYear1 = ['==', ['number', ['get', 'year2']], (year)];
     // main plants
@@ -51,11 +51,8 @@ map.on('load', function() {
     // filter for announced
     var filterStatus4 = ['==', ['string', ['get', 'status']], 'Announced'];
 
-
-   //var filterType = ['!=', ['string', ['get','type']], 'placeholder'];
-
    map.addLayer({
-    id: 'closingsoon',
+    id: 'closing',
     type: 'circle',
     source: {
         type: 'geojson',
@@ -87,27 +84,20 @@ map.on('load', function() {
         paint: {
             'circle-radius': {
                 property: 'capacity',
-                type: 'exponential', // exponential scales in mapbox default to 1 which is a linear scale
-                // found that a linear scale seemed to work better for this than for the UK power map
+                type: 'exponential',
                 stops: [
                   [50, 4],
                   [6720, 30]
                 ]
               },
           'circle-color': '#ffc83e',
-        //   [
-        //     'match',
-        //     (['get', 'year1'] == year), '#ced1cc',
-        //     (['get', 'year2'] == year), '#ea545c',
-        //     /* other */ '#ffc83e'
-        //   ],
           'circle-opacity': 0.45
         },
         'filter': ['all', filterStartYear2, filterEndYear2, filterYear5]    // filter for start and end year AND make sure that start year is less than 2018 (filterYear5)
       });
 
       map.addLayer({
-        id: 'newlyopened',
+        id: 'new',
         type: 'circle',
         source: {
             type: 'geojson',
@@ -116,8 +106,7 @@ map.on('load', function() {
         paint: {
             'circle-radius': {
                 property: 'capacity',
-                type: 'exponential', // exponential scales in mapbox default to 1 which is a linear scale
-                // found that a linear scale seemed to work better for this than for the UK power map
+                type: 'exponential',
                 stops: [
                   [50, 4],
                   [6720, 30]
@@ -139,8 +128,7 @@ map.on('load', function() {
         paint: {
             'circle-radius': {
                 property: 'capacity',
-                type: 'exponential', // exponential scales in mapbox default to 1 which is a linear scale
-                // found that a linear scale seemed to work better for this than for the UK power map
+                type: 'exponential',
                 stops: [
                   [50, 4],
                   [6720, 30]
@@ -149,7 +137,7 @@ map.on('load', function() {
           'circle-color': '#dd54b6',
           'circle-opacity': 0.45
             },
-            'filter': ['all', filterYear4, filterYear3, filterStatus1] // filter so that start year is greater than or equal to 2018 AND the slider is set to 2018
+        'filter': ['all', filterYear4, filterYear3, filterStatus1] 
        })
 
        map.addLayer({
@@ -162,8 +150,7 @@ map.on('load', function() {
         paint: {
             'circle-radius': {
                 property: 'capacity',
-                type: 'exponential', // exponential scales in mapbox default to 1 which is a linear scale
-                // found that a linear scale seemed to work better for this than for the UK power map
+                type: 'exponential',
                 stops: [
                   [50, 4],
                   [6720, 30]
@@ -172,7 +159,7 @@ map.on('load', function() {
           'circle-color': '#dd54b6',
           'circle-opacity': 0.35
             },
-            'filter': ['all', filterYear4, filterYear3, filterStatus2] // filter so that start year is greater than or equal to 2018 AND the slider is set to 2018
+            'filter': ['all', filterYear4, filterYear3, filterStatus2] 
        })
 
        map.addLayer({
@@ -185,8 +172,7 @@ map.on('load', function() {
         paint: {
             'circle-radius': {
                 property: 'capacity',
-                type: 'exponential', // exponential scales in mapbox default to 1 which is a linear scale
-                // found that a linear scale seemed to work better for this than for the UK power map
+                type: 'exponential',
                 stops: [
                   [50, 4],
                   [6720, 30]
@@ -195,7 +181,7 @@ map.on('load', function() {
           'circle-color': '#dd54b6',
           'circle-opacity': 0.25
             },
-            'filter': ['all', filterYear4, filterYear3, filterStatus3] // filter so that start year is greater than or equal to 2018 AND the slider is set to 2018
+            'filter': ['all', filterYear4, filterYear3, filterStatus3] 
        })
 
        map.addLayer({
@@ -208,8 +194,7 @@ map.on('load', function() {
         paint: {
             'circle-radius': {
                 property: 'capacity',
-                type: 'exponential', // exponential scales in mapbox default to 1 which is a linear scale
-                // found that a linear scale seemed to work better for this than for the UK power map
+                type: 'exponential',
                 stops: [
                   [50, 4],
                   [6720, 30]
@@ -218,7 +203,7 @@ map.on('load', function() {
           'circle-color': '#dd54b6',
           'circle-opacity': 0.15
             },
-            'filter': ['all', filterYear4, filterYear3, filterStatus4] // filter so that start year is greater than or equal to 2018 AND the slider is set to 2018
+            'filter': ['all', filterYear4, filterYear3, filterStatus4] 
        })
 
       // update hour filter when the slider is dragged
@@ -231,64 +216,205 @@ map.on('load', function() {
         filterYear3 = ['==', ['number', ['get', 'year1']], year];
         // update the map
         map.setFilter('powerplants', ['all', filterStartYear2, filterEndYear2, filterYear5]); //the filter only applies to the powerplants layer
-        map.setFilter('closingsoon', ['all', filterYear1, filterYear5]);
-        map.setFilter('newlyopened', ['all', filterYear3, filterYear5]);
+        map.setFilter('closing', ['all', filterYear1, filterYear5]);
+        map.setFilter('new', ['all', filterYear3, filterYear5]);
+        // four different layers for the different planned opacities
         map.setFilter('planned1', ['all', filterYear4, filterYear3, filterStatus1]);
         map.setFilter('planned2', ['all', filterYear4, filterYear3, filterStatus2]);
         map.setFilter('planned3', ['all', filterYear4, filterYear3, filterStatus3]);
         map.setFilter('planned4', ['all', filterYear4, filterYear3, filterStatus4]);
   
-        // update text in the UI
+        // update text in the UI. Use getYear array to ensure that 2018 displays as 'future'
         document.getElementById('active-hour').innerText = getYear[year];
     });
 
     // Create a popup, but don't add it to the map yet.
-  var popup = new mapboxgl.Popup({
-    closeButton: false,
-    closeOnClick: false
-  });
+    var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    });
 
-  map.on('mouseenter', 'powerplants', function(e) {
-    // Change the cursor style as a UI indicator.
-    map.getCanvas().style.cursor = 'pointer';
+    // tried to create one function for all layers but referencing multiple layers doesn't seem possible
+    map.on('mouseenter', 'powerplants', function(e) {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
 
-    var colorsArray = {
-      "Coal": "#ced1cc",
-      "Gas": "#4e80e5",
-      "Solar": "#ffc83e",
-      "Nuclear": "#dd54b6",
-      "Oil": "#a45edb",
-      "Hydro": "#43cfef",
-      "Wind": "#00a98e",
-      "Biomass": "#A7B734",
-      "Waste": "#ea545c",
-      "Other": "#cc9b7a"
-    }
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var name = e.features[0].properties.plant;
+        var capacity = e.features[0].properties.capacity;
+        var coalType = e.features[0].properties.coalType;
 
-    var coordinates = e.features[0].geometry.coordinates.slice();
-    var name = e.features[0].properties.plant;
-    var capacity = e.features[0].properties.capacity;
-    var coalType = e.features[0].properties.coalType;
-    // match plant type to the color in colorsArray, so that the title of the tooltip changes color
-    var plantColor = colorsArray[e.features[0].properties.type]; 
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
 
-    // Ensure that if the map is zoomed out such that multiple
-    // copies of the feature are visible, the popup appears
-    // over the copy being pointed to.
-    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-    }
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup.setLngLat(coordinates)
+            .setHTML('<h3 style = "color: #ffc83e;">' + name + '</h3><p>Capacity: <b>' + capacity + ' MW</b></p><p>Type: <b>' + coalType + '</b></p>')
+            .addTo(map);
 
-    // Populate the popup and set its coordinates
-    // based on the feature found.
-    popup.setLngLat(coordinates)
-        .setHTML('<h3 style = "color: ' + plantColor + ';">' + name + '</h3><p>Capacity: <b>' + capacity + ' MW</b></p><p>Type: <b>' + coalType + '</b></p>')
-        .addTo(map);
+    });
+
+    map.on('mouseenter', 'closing', function(e) {
+        map.getCanvas().style.cursor = 'pointer';
+
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var name = e.features[0].properties.plant;
+        var capacity = e.features[0].properties.capacity;
+        var coalType = e.features[0].properties.coalType;
+
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        popup.setLngLat(coordinates)
+            .setHTML('<h3 style = "color: #ff8767;">' + name + '</h3><p>Capacity: <b>' + capacity + ' MW</b></p><p>Type: <b>' + coalType + '</b></p>')
+            .addTo(map);
+
+    });
+
+    map.on('mouseenter', 'new', function(e) {
+        map.getCanvas().style.cursor = 'pointer';
+
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var name = e.features[0].properties.plant;
+        var capacity = e.features[0].properties.capacity;
+        var coalType = e.features[0].properties.coalType;
+
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        popup.setLngLat(coordinates)
+            .setHTML('<h3 style = "color: #ced1cc;">' + name + '</h3><p>Capacity: <b>' + capacity + ' MW</b></p><p>Type: <b>' + coalType + '</b></p>')
+            .addTo(map);
+
+    });
+
+    map.on('mouseenter', 'planned1', function(e) {
+        map.getCanvas().style.cursor = 'pointer';
+
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var name = e.features[0].properties.plant;
+        var capacity = e.features[0].properties.capacity;
+        var coalType = e.features[0].properties.coalType;
+
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        popup.setLngLat(coordinates)
+            .setHTML('<h3 style = "color: #dd54b6;">' + name + '</h3><p>Capacity: <b>' + capacity + ' MW</b></p><p>Type: <b>' + coalType + '</b></p>')
+            .addTo(map);
+
+    });
+
+    map.on('mouseenter', 'planned2', function(e) {
+        map.getCanvas().style.cursor = 'pointer';
+
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var name = e.features[0].properties.plant;
+        var capacity = e.features[0].properties.capacity;
+        var coalType = e.features[0].properties.coalType;
+
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        popup.setLngLat(coordinates)
+            .setHTML('<h3 style = "color: #dd54b6;">' + name + '</h3><p>Capacity: <b>' + capacity + ' MW</b></p><p>Type: <b>' + coalType + '</b></p>')
+            .addTo(map);
+
+    });
+
+    map.on('mouseenter', 'planned3', function(e) {
+        map.getCanvas().style.cursor = 'pointer';
+
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var name = e.features[0].properties.plant;
+        var capacity = e.features[0].properties.capacity;
+        var coalType = e.features[0].properties.coalType;
+
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        popup.setLngLat(coordinates)
+            .setHTML('<h3 style = "color: #dd54b6;">' + name + '</h3><p>Capacity: <b>' + capacity + ' MW</b></p><p>Type: <b>' + coalType + '</b></p>')
+            .addTo(map);
+
+    });
+
+    map.on('mouseenter', 'planned4', function(e) {
+        map.getCanvas().style.cursor = 'pointer';
+
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var name = e.features[0].properties.plant;
+        var capacity = e.features[0].properties.capacity;
+        var coalType = e.features[0].properties.coalType;
+
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        popup.setLngLat(coordinates)
+            .setHTML('<h3 style = "color: #dd54b6;">' + name + '</h3><p>Capacity: <b>' + capacity + ' MW</b></p><p>Type: <b>' + coalType + '</b></p>')
+            .addTo(map);
+
     });
 
     map.on('mouseleave', 'powerplants', function() {
+
         map.getCanvas().style.cursor = '';
         popup.remove();
+
     });
+
+    map.on('mouseleave', 'closing', function() {
+
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+
+    });
+
+    map.on('mouseleave', 'new', function() {
+
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+
+    });
+
+    map.on('mouseleave', 'planned1', function() {
+
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+
+    });
+
+    map.on('mouseleave', 'planned2', function() {
+
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+
+    });
+
+    map.on('mouseleave', 'planned3', function() {
+
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+
+    });
+
+    map.on('mouseleave', 'planned4', function() {
+
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+
+    });
+
 
 });
