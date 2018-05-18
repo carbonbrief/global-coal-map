8168d78e4f921af3507791c4d1fa124d07055aaf
Rosamund Pearce <rosamundpearce@live.com>
2018-05-18 16:07:24 +0100

    Revert "change basemap"
    This reverts commit e619024def0e05bbd798a69e350307511453fff8.

--- js/script.js
+++ js/script.js
@@ -85,7 +85,7 @@ map.on('load', function() {
             ]
           },
       'circle-color': '#ced1cc',
-      'circle-opacity': 0.5
+      'circle-opacity': 0.45
         },
         'filter': ['all', filterClosing, filterRegion]
    })
@@ -107,7 +107,7 @@ map.on('load', function() {
                 ]
               },
           'circle-color': '#ffc83e',
-          'circle-opacity': 0.5
+          'circle-opacity': 0.45
         },
         'filter': ['all', filterOperating, filterOperating2, filterRegion]    // filter for start and end year AND make sure that start year is less than 2018 (filterYear5)
       });
@@ -129,7 +129,7 @@ map.on('load', function() {
                 ]
               },
           'circle-color': '#ff8767',
-          'circle-opacity': 0.5
+          'circle-opacity': 0.45
             },
             'filter': ['all', filterNew, filterRegion]
        })
@@ -151,7 +151,7 @@ map.on('load', function() {
                 ]
               },
           'circle-color': '#a45edb',
-          'circle-opacity': 0.4
+          'circle-opacity': 0.35
             },
             'filter': ['all', filterFuture, filterPlanned, filterRegion] 
        })
@@ -173,7 +173,7 @@ map.on('load', function() {
                 ]
               },
           'circle-color': '#dd54b6',
-          'circle-opacity': 0.5
+          'circle-opacity': 0.45
             },
         'filter': ['all', filterFuture, filterConstruction, filterRegion] 
        })
